9032fe25e2baeea002951c25cac307a31ddc11d2
OBeeNya <benjamin.aubignat@gmail.com>
2022-10-05 18:17:12 +0200

    ex03: WIP

---
+++ ex03/DiamondTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef DIAMONDTRAP_HPP
+# define DIAMONDTRAP_HPP
+
+# include "FragTrap.hpp"
+# include "ScavTrap.hpp"
+
+class DiamondTrap:
+	public FragTrap,
+	public ScavTrap
+{
+	public:
+	
+
+
+	private:
+
+
+
+};
+
+#endif
---
+++ ex03/FragTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap:
+	public ClapTrap
+{
+	public:
+
+		FragTrap(void);
+		FragTrap(const FragTrap &src);
+		FragTrap(const std::string name);
+		~FragTrap(void);
+
+		FragTrap	&operator=(const FragTrap &rhs);
+
+		virtual void	attack(const std::string &target);
+		virtual void	takeDamage(unsigned int amount);
+		virtual void	beRepaired(unsigned int amount);
+		void			highFiveGuys(void);
+
+	private:
+
+};
+
+#endif
